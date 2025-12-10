303a982e07284170b125af5981af58555eed4195
bbangs <bbangs@smbholdings.co.kr>
2025-12-10 16:20:44 +0900

    yml 수정후에 파일 수정해서 푸시

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 5000;
 
 const msg = `
   <p>지금부터 수정할꺼야. 그럼 이제 <span style='color:red'>새로운 이미지가</span> 도커허브에 올라가겠지???</p>
-  <p>실패...secret 다시 설정하고 한번 더!!!!</p>
+  <p>혹시 모르니... 파일 수정후에 푸시해볼께</p>
 `;
 
 const server = http.createServer((req, res) => {
